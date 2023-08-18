f45664289838ca40c5829f438529c9427edcb3c5
Sole <barboza.soledad@gmail.com>
2023-08-18 13:49:23 -0300

    Pairing Exercise 2: For Statements. Solved.

---
+++ pairing/friends.js
@@ -0,0 +1,7 @@
+var numbers = [3, 5, 22, 58, 34, 60, 88, 99];
+console.log("Números menores de 30:");
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 30) {
+        console.log(numbers[i]);
+    }
+}
