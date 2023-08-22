b6cc2474f8dc904ef1fb379c9c82086af1ee64b3
Sole <barboza.soledad@gmail.com>
2023-08-22 14:01:55 -0300

    I add second part of exercise 2.

--- pairing/friends.js
+++ pairing/friends.js
@@ -1,7 +1,15 @@
 var numbers = [3, 5, 22, 58, 34, 60, 88, 99];
-console.log("Números menores de 30:");
+console.log("Numbers above 30:");
 for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] < 30) {
+    if (numbers[i] > 30) {
+        console.log(numbers[i]);
+    }
+}
+
+var numbers = [3, 5, 22, 58, 34, 60, 88, 99];
+console.log("Numbers under 60:");
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 60) {
         console.log(numbers[i]);
     }
 }
